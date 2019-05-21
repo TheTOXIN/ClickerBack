0eb1bdce34a54cc8dc6aa281adab22e9c9c16eeb
toxin <yuri.belousov.1998@gmail.com>
2019-05-21 15:04:11 +0300

    Initial commit

---
+++ src/main/java/com/toxin/clickerback/ClickerbackApplication.java
@@ -0,0 +1,12 @@
+package com.toxin.clickerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickerbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClickerbackApplication.class, args);
+    }
+}
---
+++ src/test/java/com/toxin/clickerback/ClickerbackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toxin.clickerback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ClickerbackApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
